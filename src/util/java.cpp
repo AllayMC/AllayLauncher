57798acfcbfbb650cd85b09bf51af6f99c98c3b6
daoge_cmd <3523206925@qq.com>
2024-11-16 15:44:07 +0800

    fix(build): workaround to fix lint issue on windows platform

--- src/util/java.cpp
+++ src/util/java.cpp
@@ -9,6 +9,8 @@ std::expected<Version, GetOSJavaVersionError> installed_version() {
     using namespace string;
 
     // Do not use "java --version", which is not supported on java-1.8
+    // The output of this command will be written into error stream
+    // so we need to add the suffix "2>&1" to redirect the output
     auto output = os::execute("java -version 2>&1");
 
     if (!output) return std::unexpected(GetOSJavaVersionError::ExecuteCommandFailed);
