af9a79a8c3e1cf6c55ea8942c94158a7545223f8
epicbigcat <184706652+epicbigcat@users.noreply.github.com>
2024-11-18 23:18:31 +0800

    chore: optimize argument processing.

--- src/util/os.cpp
+++ src/util/os.cpp
@@ -2,11 +2,15 @@
 
 #include "internal/popen.h"
 
+#include <cstdio>
+
 namespace allay_launcher::util::os {
 
 using namespace internal;
 
 std::expected<std::string, CommandExecutionError> execute(std::string_view command) {
+    logging::debug("execute(): {}", command);
+
     Popen pipe(command, "r");
     if (!pipe.get()) return std::unexpected(CommandExecutionError::CreatePipeError);
 
@@ -15,6 +19,12 @@ std::expected<std::string, CommandExecutionError> execute(std::string_view comma
     while (fgets(buffer.data(), buffer.size(), pipe.get())) ret += buffer.data();
 
     return ret;
+}
+
+void system(std::string_view command) {
+    logging::debug("system(): {}", command);
+
+    ::system(command.data());
 }
 
 } // namespace allay_launcher::util::os
