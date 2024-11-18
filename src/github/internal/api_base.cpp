52fc07f00bcee854b9afd9e43c51d073c87bf95d
epicbigcat <184706652+epicbigcat@users.noreply.github.com>
2024-11-18 21:32:09 +0800

    fix: fix bad_weak_ptr error.

--- src/github/internal/api_base.cpp
+++ src/github/internal/api_base.cpp
@@ -3,11 +3,11 @@
 namespace allay_launcher::github {
 
 std::shared_ptr<cpr::Session> ApiBase::create_session() const {
-    cpr::Session session;
+    auto session = std::make_shared<cpr::Session>();
 
     // TODO: Should we use GetSharedPtrFromThis?
     //       see: https://github.com/libcpr/cpr/issues/1145
-    return session.shared_from_this();
+    return session->shared_from_this();
 }
 
 } // namespace allay_launcher::github
