893ca569794d427f914cfbd33b8f6b6da0601054
daoge_cmd <3523206925@qq.com>
2024-11-26 21:36:51 +0800

    fix: fix install_linux.sh

---
+++ scripts/install_linux.sh
@@ -0,0 +1,3 @@
+wget -O allay https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-x64-release
+chmod +x allay
+allay
--- scripts/installl_linux.sh
+++
@@ -1,3 +0,0 @@
-wget -O https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-x64-release allay
-chmod +x allay
-allay
