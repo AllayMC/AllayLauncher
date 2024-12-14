716989e07511369a1501d61187b05c6057bdab19
daoge_cmd <3523206925@qq.com>
2024-12-14 23:40:38 +0800

    feat: check architecture type in install_linux.sh

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,3 +1,32 @@
-wget -O allay https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-x64-release
+ARCH=$(uname -m)
+URL=""
+
+case "$ARCH" in
+    "x86_64")
+        URL="https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-x64-release"
+        ;;
+    "aarch64")
+        URL="https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-arm64-v8a-release"
+        ;;
+    "mips64")
+        URL="https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-mips64-release"
+        ;;
+    "mips64el")
+        URL="https://github.com/AllayMC/AllayLauncher/releases/latest/download/allay-launcher-linux-mips64el-release"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+wget -O allay "$URL"
+if [ $? -eq 0 ]; then
+    chmod +x allay
+else
+    echo "Failed to download file. Please check your network."
+    exit 1
+fi
+
 chmod +x allay
 ./allay
