2785ed987e94b0ee938d34704c1a6e36d982cec9
SoHugePenguin <100383563+SoHugePenguin@users.noreply.github.com>
2025-01-07 23:27:31 +0800

    fix: Fix java version recognition and console prompts

--- src/base/version.cpp
+++ src/base/version.cpp
@@ -6,17 +6,24 @@ namespace allay_launcher {
 
 std::optional<Version> Version::parse(std::string_view str) {
     auto tokens = util::string::split(str, ".");
-    if (tokens.size() < 3) {
+    if (tokens.size() > 3) {
         return {};
     }
 
-    auto major    = util::string::to_int32(tokens.at(0));
-    auto minor    = util::string::to_int32(tokens.at(1));
-    auto revision = util::string::to_int32(tokens.at(2));
+    // major(require),minor,revision
+    std::vector<std::optional<int32_t>> version = {0, 0, 0};
 
-    if (!major || !minor || !revision) return {};
+    auto vId = 0;
+    for (const auto& item : tokens) {
+        version[vId] = util::string::to_int32(tokens[vId]);
+        vId++;
+    }
 
-    return Version{*major, *minor, *revision};
+    if (!version[0]) return {};
+    auto ret = Version{*version[0], 0, 0};
+    if (version[1]) ret.m_minor = *version[1];
+    if (version[2]) ret.m_revision = *version[2];
+    return ret;
 }
 
 } // namespace allay_launcher
