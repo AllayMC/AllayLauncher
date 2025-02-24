844f170e338f7515fa14891debbbed1478ca810d
epicbigcat <184706652+epicbigcat@users.noreply.github.com>
2025-02-24 17:28:58 +0800

    refactor: try inline str operations.

--- src/base/global.h
+++ src/base/global.h
@@ -9,3 +9,7 @@ namespace allay_launcher::global {
 std::shared_ptr<cpr::Session> CreateCprSession();
 
 } // namespace allay_launcher::global
+
+// Predefines
+
+#define AL_INLINE inline
--- src/util/string.h
+++ src/util/string.h
@@ -2,17 +2,52 @@
 
 namespace allay_launcher::util::string {
 
-bool starts_with(const std::string& str, const std::string& prefix);
+AL_INLINE bool starts_with(const std::string& str, const std::string& prefix) {
+    return prefix.size() <= str.size() && str.compare(0, prefix.size(), prefix) == 0;
+}
 
-bool ends_with(const std::string& str, const std::string& suffix);
+AL_INLINE bool ends_with(const std::string& str, const std::string& suffix) {
+    return suffix.size() <= str.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
 
-void remove_prefix(std::string& str, const std::string& prefix);
+AL_INLINE void remove_prefix(std::string& str, const std::string& prefix) {
+    if (str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0) {
+        str.erase(0, prefix.size());
+    }
+}
 
-void remove_suffix(std::string& str, const std::string& suffix);
+AL_INLINE void remove_suffix(std::string& str, const std::string& suffix) {
+    if (str.size() >= suffix.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0) {
+        str.erase(str.size() - suffix.size());
+    }
+}
 
-std::optional<int32_t> to_int32(std::string_view str);
+AL_INLINE std::optional<int32_t> to_int32(std::string_view str) {
+    char* str_end = nullptr;
+    errno         = 0;
 
-std::vector<std::string> split(std::string_view str, std::string_view delimiter);
+    long value = std::strtol(str.data(), &str_end, 0);
+
+    if (str_end == str.data() || errno == ERANGE || *str_end != '\0') {
+        return {};
+    }
+
+    return value;
+}
+
+AL_INLINE std::vector<std::string> split(std::string_view str, std::string_view delimiter) {
+    std::vector<std::string> tokens;
+    size_t                   start = 0, end = 0;
+
+    while ((end = str.find(delimiter, start)) != std::string::npos) {
+        tokens.emplace_back(str.substr(start, end - start));
+        start = end + delimiter.length();
+    }
+
+    tokens.emplace_back(str.substr(start));
+
+    return tokens;
+}
 
 // from: https://stackoverflow.com/questions/2111667/compile-time-string-hashing
 constexpr unsigned H(char const* input) {
@@ -20,7 +55,7 @@ constexpr unsigned H(char const* input) {
 }
 
 #ifdef _WIN32
-std::string from_wstring(const std::wstring& wstr);
+AL_INLINE std::string from_wstring(const std::wstring& wstr) { return std::filesystem::path(wstr).string(); }
 #endif
 
 } // namespace allay_launcher::util::string
